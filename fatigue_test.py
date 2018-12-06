c8748f52f2ca52b3641c6c9e2767481453da3585
Richard Bowman <richard.bowman@cantab.net>
2018-12-06 11:26:44 +0000

    updated analysis code
    plotting X and Y (microscope motors might be swapped) and also an XY graph.  Improved acquisition code - but that's not yet tested.

--- fatigue_test.py
+++ fatigue_test.py
@@ -41,34 +41,53 @@ if __name__ == "__main__":
         stage.backlash = backlash
 
         data_group = df.new_group("fatigue_test")
+        data_group.attrs['step_size'] = step_size
+        data_group.attrs['n_steps'] = n_steps
+        data_group.attrs['backlash'] = backlash
+        data_group.attrs['camera_resolution'] = np.array(camera.resolution)
 
         camera.start_preview(resolution=(640,480))
 
         #stage.move_rel([-backlash, -backlash, -backlash])
         #stage.move_rel([backlash, backlash, backlash])
+        data_group['initial_sample_image'] = ms.rgb_image()
 
         frame = ms.rgb_image().astype(np.float32).mean(axis=2)
         #mean = np.mean(image)
         #templ8 = (image - mean)[100:-100, 100:-100]
         template = np.load("template.npz")['template']
+        
+        data_group['template_image'] = template
 
         d, corr = find_template(template, frame, return_corr = True, fraction=0.15)        
 
         initial_stage_position = stage.position
+        start_time = time.time()
+        data_group.attrs['start_time'] = start_time
+        last_saved_image_time = 0
 
         try:
             while True:
-                for d in [1, -1]:
+                for d in [1, 0, -1, 0]:
                     data_stage = np.zeros((n_steps + 1, 3))
                     data_cam = np.zeros((n_steps + 1, 2))
+                    data_time = np.zeros((n_steps+1,))
                     for i in range(n_steps+1):
                         if i>0:
                             stage.move_rel([d*step_size, 0, 0])
                         data_stage[i, 0:] = stage.position
                         frame = ms.rgb_image().astype(np.float32).mean(axis=2)
                         data_cam[i, 0:], corr = find_template(template, frame, return_corr = True, fraction=0.15)
-                    df.add_data(data_stage, data_group, "data_stage_{}".format("out" if d==1 else "back"))
-                    df.add_data(data_cam, data_group, "data_cam_{}".format("out" if d==1 else "back"))
+                        data_time[i] = time.time()
+                    df.add_data(data_stage, data_group, "data_stage")
+                    df.add_data(data_cam, data_group, "data_cam")
+                    df.add_data(data_time, data_group, "data_time")
+                if time.time() - last_saved_image_time > 900:
+                    # periodically save a JPEG of what the camera sees.  NB we could do this more
+                    # efficiently by capturing JPEG directly - but it's hardly time critical.
+                    frame = cam.rgb_image()
+                    df.add_data(np.array(cv2.imencode('.jpg', frame)[1]), data_group, "image")
+                    last_saved_image_time = time.time()
         except KeyboardInterrupt:
             print("Got a keyboard interrupt, stopping...")
 
--- fatigue_test_plot.py
+++ fatigue_test_plot.py
@@ -14,17 +14,27 @@ if __name__ == "__main__":
     dset_names = data_group.keys()
     print(dset_names)
 
-    f, ax = plt.subplots(2,1)
-
-    for i in range(len(dset_names)//4):
+    N = len(dset_names)//4 # this is the number of out-and-backs we did
+    first_move = data_group['data_cam_out00000']
+    m = first_move.shape[0] # the number of data points in each move
+    
+    # let's combine the datasets
+    cam_coords = np.empty((2*N*m, 2))
+    stage_coords = np.empty_like(cam_coords)
+    
+    f, ax = plt.subplots(2,1, sharex=True)
+    f2, ax2 = plt.subplots(1,1)
+    xs = []
+    for i in range(N-1): # ignore the last move, it may not have completed properly
         for d in ['out', 'back']:
             data_cam = data_group['data_cam_{}{:05d}'.format(d,i)]
             data_stage = data_group['data_stage_{}{:05d}'.format(d,i)]
-            N = data_cam.shape[0]
-            x = np.arange(N) + 2*i*N
-            if d == 'back':
-                x += N
-            ax[0].plot(x,data_cam[:,0])
-            ax[1].plot(x,data_stage[:,0])
+            x = np.arange(m, dtype=np.int) + 2*i*m + (m if d=="back" else 0)
+            rr = slice(np.min(x), np.max(x)+1)
+            cam_coords[rr, :] = data_cam[:,:2]
+            stage_coords[rr, :] = data_stage[:,:2]
+            for j in range(2):
+                ax[j].plot(x,data_cam[:,j])
+            ax2.plot(data_cam[:,0], data_cam[:,1])
             
     plt.show()
